fd34b33193f8e314560967b88b70f478ee5e27e5
dunas26 <jhonjamesillo@gmail.com>
2022-04-10 23:58:38 -0500

    Agregamos el enrutador a la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { AcercaNosotrosComponent } from './components/acerca-nosotros/acerca-nosotros.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaNosotrosComponent } from './components/acerca-nosotros/acerca-nos
     AcercaNosotrosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
