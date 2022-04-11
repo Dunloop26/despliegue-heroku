d3bc16b090863b90385c94e61744bc03f10e2135
dunas26 <jhonjamesillo@gmail.com>
2022-04-11 00:15:11 -0500

    Configuracion de ruta para componente acerca de nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { AcercaNosotrosComponent } from './components/acerca-nosotros/acerca-nosotros.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "inicio", pathMatch: "full" },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: AcercaNosotrosComponent }
 ]
 
 @NgModule({
